17cbe4b1dfb6e7f319a295a130de2a7ab9c2803e
Vera Kasianenko <112972833+CandyGoose@users.noreply.github.com>
2024-09-19 02:53:31 +0300

    L1.12

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,14 @@
-use std::collections::HashMap;
+use std::collections::HashSet;
 
-fn group_temperatures(temps: Vec<f64>) -> HashMap<i32, Vec<f64>> {
-    let mut grouped_temps: HashMap<i32, Vec<f64>> = HashMap::new();
-
-    for &temp in temps.iter() {
-        let lower_bound = (temp / 10.0).floor() as i32 * 10;
-        grouped_temps.entry(lower_bound).or_insert(Vec::new()).push(temp);
-    }
-
-    for temps in grouped_temps.values_mut() {
-        temps.sort_by(|a, b| a.partial_cmp(b).unwrap());
-    }
-
-    grouped_temps
+fn intersection(set1: &HashSet<i32>, set2: &HashSet<i32>) -> HashSet<i32> {
+    set1.intersection(set2).cloned().collect()
 }
 
 fn main() {
-    let temperatures = vec![-25.4, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5];
+    let set1: HashSet<i32> = vec![5, 22, 32, 41, 1].into_iter().collect();
+    let set2: HashSet<i32> = vec![1, 46, 5, 621, 73].into_iter().collect();
 
-    let result = group_temperatures(temperatures);
+    let result = intersection(&set1, &set2);
 
-    for (interval, temps) in result.iter() {
-        println!("[{}, {}): {:?}", interval, interval + 10, temps);
-    }
+    println!("Intersection: {:?}", result);
 }
