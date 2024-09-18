77c8076c9db4894deab85ad443f397ca24bce62f
Vera Kasianenko <112972833+CandyGoose@users.noreply.github.com>
2024-09-19 00:49:46 +0300

    L1.4

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,42 @@
+use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
-use std::sync::mpsc;
+use std::time::Duration;
 
 fn main() {
-    let n = 10;
-    let numbers: Vec<i32> = (1..=n).collect();
+    let worker_count = 4;
 
     let (tx, rx) = mpsc::channel();
 
-    for num in numbers {
-        let tx = tx.clone();
-        thread::spawn(move || {
-            let square = num * num;
-            tx.send(square).unwrap();
+    let rx = Arc::new(Mutex::new(rx));
+
+    thread::spawn(move || {
+        let mut counter = 0;
+        loop {
+            counter += 1;
+            tx.send(format!("Message {}", counter)).unwrap();
+            thread::sleep(Duration::from_millis(500));
+        }
+    });
+
+    let mut workers = vec![];
+    for i in 0..worker_count {
+        let rx = Arc::clone(&rx);
+        let worker_id = i + 1;
+        let handle = thread::spawn(move || {
+            loop {
+                let message = rx.lock().unwrap().recv();
+                match message {
+                    Ok(message) => {
+                        println!("Worker {} received: {}", worker_id, message);
+                    }
+                    Err(_) => break,
+                }
+            }
         });
+        workers.push(handle);
     }
 
-    let mut sum = 0;
-    for _ in 1..=n {
-        sum += rx.recv().unwrap();
+    for worker in workers {
+        worker.join().unwrap();
     }
-
-    println!("Sum of squares: {}", sum);
 }
