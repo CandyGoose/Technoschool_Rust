d35f5636e3a548100bde5140c3456caec0a11dde
Goose <112972833+CandyGoose@users.noreply.github.com>
2024-09-19 01:30:28 +0300

    L1.6

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 use flume::unbounded;
 use tokio::time::{self, Duration};
+use tokio_util::sync::CancellationToken;
+use std::sync::Arc;
 
 #[tokio::main]
 async fn main() {
     let (tx, rx) = unbounded();
-
     let n_seconds = 5;
-
     let tx_clone = tx.clone();
+    let cancel_token = Arc::new(CancellationToken::new());
 
+    let cancel_token_clone = cancel_token.clone();
     let sender_task = tokio::spawn(async move {
         let mut counter = 0;
         let timeout = time::sleep(Duration::from_secs(n_seconds));
@@ -27,17 +29,37 @@ async fn main() {
                     }
                     println!("Sent: {}", counter);
                 }
+                _ = cancel_token_clone.cancelled() => {
+                    println!("Sender task cancelled.");
+                    break;
+                }
             }
         }
     });
 
+    let cancel_token_clone = cancel_token.clone();
     let receiver_task = tokio::spawn(async move {
-        while let Ok(value) = rx.recv_async().await {
-            println!("Received: {}", value);
+        loop {
+            tokio::select! {
+                message = rx.recv_async() => {
+                    if let Ok(value) = message {
+                        println!("Received: {}", value);
+                    } else {
+                        println!("Channel closed, stopping receiver.");
+                        break;
+                    }
+                }
+                _ = cancel_token_clone.cancelled() => {
+                    println!("Receiver task cancelled.");
+                    break;
+                }
+            }
         }
     });
 
     time::sleep(Duration::from_secs(n_seconds)).await;
+
+    cancel_token.cancel();
 
     drop(tx);
 
