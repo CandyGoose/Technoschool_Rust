72a49378d13399426bcdc8fbd5f860bd39b4d14c
Vera Kasianenko <112972833+CandyGoose@users.noreply.github.com>
2024-09-18 17:30:12 +0300

    L1.1

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+trait Action {
+    fn say(&self);
+}
+
+struct Person {
+    name: String,
+}
+
+impl Action for Person {
+    fn say(&self) {
+        println!("Hello, {}", self.name);
+    }
+}
+
+fn main() {
+    let person = Person {
+        name: String::from("Alice"),
+    };
+
+    person.say();
+}
