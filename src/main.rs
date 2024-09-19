1c5f4b0f7c4530cbe191c958de8778a15d9ec07c
Vera Kasianenko <112972833+CandyGoose@users.noreply.github.com>
2024-09-19 03:29:42 +0300

    L1.15

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,30 @@
-use std::any::type_name;
+fn quicksort(arr: &mut [i32]) {
+    if arr.len() <= 1 {
+        return;
+    }
 
-fn print_type_of<T>(_: &T) {
-    println!("The type of the variable is: {}", type_name::<T>());
+    let pivot_index = partition(arr);
+    quicksort(&mut arr[0..pivot_index]);
+    quicksort(&mut arr[pivot_index + 1..]);
+}
+
+fn partition(arr: &mut [i32]) -> usize {
+    let pivot = arr[arr.len() - 1];
+    let mut i = 0;
+
+    for j in 0..arr.len() - 1 {
+        if arr[j] < pivot {
+            arr.swap(i, j);
+            i += 1;
+        }
+    }
+    arr.swap(i, arr.len() - 1);
+    i
 }
 
 fn main() {
-    let x = 1;
-    let y = 1.1;
-    let z = "hello";
-
-    print_type_of(&x);
-    print_type_of(&y);
-    print_type_of(&z);
+    let mut array = [134, 37, 223, 312, 15, 642, 3];
+    println!("Before sorting: {:?}", array);
+    quicksort(&mut array);
+    println!("After sorting: {:?}", array);
 }
