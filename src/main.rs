4a06f8011a5a115ced1a854212e675f55669146f
Vera Kasianenko <112972833+CandyGoose@users.noreply.github.com>
2024-09-19 04:04:08 +0300

    L1.18

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,10 @@
-use std::sync::Arc;
-use std::sync::atomic::{AtomicUsize, Ordering};
-use std::thread;
-
-struct Counter {
-    value: AtomicUsize,
-}
-
-impl Counter {
-    fn new() -> Self {
-        Counter {
-            value: AtomicUsize::new(0),
-        }
-    }
-
-    fn increment(&self) {
-        self.value.fetch_add(1, Ordering::SeqCst);
-    }
-
-    fn get_value(&self) -> usize {
-        self.value.load(Ordering::SeqCst)
-    }
+fn reverse_string(input: &str) -> String {
+    input.chars().rev().collect()
 }
 
 fn main() {
-    let counter = Arc::new(Counter::new());
-
-    let mut handles = vec![];
-
-    // Создаем потоки
-    for _ in 0..10 {
-        let counter = Arc::clone(&counter);
-        let handle = thread::spawn(move || {
-            for _ in 0..1000 {
-                counter.increment();
-            }
-        });
-        handles.push(handle);
-    }
-
-    for handle in handles {
-        handle.join().unwrap();
-    }
-
-    println!("Final counter value: {}", counter.get_value());
+    let input = "главрыба";
+    let reversed = reverse_string(input);
+    println!("Original: {}", input);
+    println!("Reversed: {}", reversed);
 }
