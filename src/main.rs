c4e4a0fcd5fcf998b90338ae45838a9f8a0ec312
Goose <112972833+CandyGoose@users.noreply.github.com>
2024-09-19 04:17:44 +0300

    L1.20

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,41 @@
-fn reverse_words(input: &str) -> String {
-    input.split_whitespace().rev().collect::<Vec<&str>>().join(" ")
+struct CelsiusThermometer;
+
+impl CelsiusThermometer {
+    pub fn get_temperature(&self) -> f64 {
+        25.0
+    }
+}
+
+trait FahrenheitThermometer {
+    fn get_temperature_in_fahrenheit(&self) -> f64;
+}
+
+struct CelsiusToFahrenheitAdapter {
+    celsius_thermometer: CelsiusThermometer,
+}
+
+impl CelsiusToFahrenheitAdapter {
+    pub fn new(thermometer: CelsiusThermometer) -> Self {
+        Self {
+            celsius_thermometer: thermometer,
+        }
+    }
+}
+
+impl FahrenheitThermometer for CelsiusToFahrenheitAdapter {
+    fn get_temperature_in_fahrenheit(&self) -> f64 {
+        let celsius = self.celsius_thermometer.get_temperature();
+        celsius * 9.0 / 5.0 + 32.0
+    }
 }
 
 fn main() {
-    let input = "snow dog sun";
-    let reversed = reverse_words(input);
-    println!("{} — {}", input, reversed);
+    let celsius_thermometer = CelsiusThermometer;
+
+    let adapter = CelsiusToFahrenheitAdapter::new(celsius_thermometer);
+
+    println!(
+        "Temperature in Fahrenheit: {:.2}",
+        adapter.get_temperature_in_fahrenheit()
+    );
 }
