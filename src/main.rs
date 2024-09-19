4a5e1d89f13368586346cc274b87022d3027637e
Vera Kasianenko <112972833+CandyGoose@users.noreply.github.com>
2024-09-19 04:31:07 +0300

    L1.21

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,40 @@
-struct CelsiusThermometer;
+use bigdecimal::{BigDecimal};
+use num_bigint::BigInt;
+use std::str::FromStr;
 
-impl CelsiusThermometer {
-    pub fn get_temperature(&self) -> f64 {
-        25.0
-    }
-}
+fn arithmetic_operations<T>(a: T, b: T)
+where
+    T: std::ops::Add<Output = T>
+    + std::ops::Sub<Output = T>
+    + std::ops::Mul<Output = T>
+    + std::ops::Div<Output = T>
+    + PartialEq
+    + From<i32>
+    + std::fmt::Display
+    + Clone,
+{
+    println!("{} + {} = {}", a.clone(), b.clone(), a.clone() + b.clone());
+    println!("{} - {} = {}", a.clone(), b.clone(), a.clone() - b.clone());
+    println!("{} * {} = {}", a.clone(), b.clone(), a.clone() * b.clone());
 
-trait FahrenheitThermometer {
-    fn get_temperature_in_fahrenheit(&self) -> f64;
-}
-
-struct CelsiusToFahrenheitAdapter {
-    celsius_thermometer: CelsiusThermometer,
-}
-
-impl CelsiusToFahrenheitAdapter {
-    pub fn new(thermometer: CelsiusThermometer) -> Self {
-        Self {
-            celsius_thermometer: thermometer,
-        }
-    }
-}
-
-impl FahrenheitThermometer for CelsiusToFahrenheitAdapter {
-    fn get_temperature_in_fahrenheit(&self) -> f64 {
-        let celsius = self.celsius_thermometer.get_temperature();
-        celsius * 9.0 / 5.0 + 32.0
+    if b != T::from(0) {
+        println!("{} / {} = {}", a.clone(), b.clone(), a.clone() / b.clone());
+    } else {
+        println!("Division by zero is not allowed!");
     }
 }
 
 fn main() {
-    let celsius_thermometer = CelsiusThermometer;
+    let a_float = BigDecimal::from_str("1000000000000000000000.123456789").unwrap();
+    let b_float = BigDecimal::from_str("2000000000000000000000.987654321").unwrap();
 
-    let adapter = CelsiusToFahrenheitAdapter::new(celsius_thermometer);
+    arithmetic_operations(a_float.clone(), b_float.clone());
 
-    println!(
-        "Temperature in Fahrenheit: {:.2}",
-        adapter.get_temperature_in_fahrenheit()
-    );
+    let a_int = BigInt::from_str("1000000000000000000").unwrap();
+    let b_int = BigInt::from_str("2000000000000000000").unwrap();
+
+    let a_int_as_float = BigDecimal::from(a_int);
+    let b_int_as_float = BigDecimal::from(b_int);
+
+    arithmetic_operations(a_int_as_float, b_int_as_float);
 }
