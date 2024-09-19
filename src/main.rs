0316c6fabe942c804cad3c3f8405ca7acbb53462
Goose <112972833+CandyGoose@users.noreply.github.com>
2024-09-19 03:07:02 +0300

    L1.13

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
-use std::collections::HashSet;
-
-fn intersection(set1: &HashSet<i32>, set2: &HashSet<i32>) -> HashSet<i32> {
-    set1.intersection(set2).cloned().collect()
-}
+use std::io::{self, BufRead};
 
 fn main() {
-    let set1: HashSet<i32> = vec![5, 22, 32, 41, 1].into_iter().collect();
-    let set2: HashSet<i32> = vec![1, 46, 5, 621, 73].into_iter().collect();
+    let stdin = io::stdin();
+    let mut unique_strings: Vec<String> = Vec::new();
 
-    let result = intersection(&set1, &set2);
+    for line in stdin.lock().lines() {
+        let line = line.unwrap();
+        if !unique_strings.contains(&line) {
+            unique_strings.push(line);
+        }
+    }
 
-    println!("Intersection: {:?}", result);
+    for string in unique_strings {
+        println!("{}", string);
+    }
 }
