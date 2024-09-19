1f7eed6c33d788f4619219bbe59d0688f26893cf
Goose <112972833+CandyGoose@users.noreply.github.com>
2024-09-19 03:11:42 +0300

    L1.14

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,15 @@
-use std::io::{self, BufRead};
+use std::any::type_name;
+
+fn print_type_of<T>(_: &T) {
+    println!("The type of the variable is: {}", type_name::<T>());
+}
 
 fn main() {
-    let stdin = io::stdin();
-    let mut unique_strings: Vec<String> = Vec::new();
+    let x = 1;
+    let y = 1.1;
+    let z = "hello";
 
-    for line in stdin.lock().lines() {
-        let line = line.unwrap();
-        if !unique_strings.contains(&line) {
-            unique_strings.push(line);
-        }
-    }
-
-    for string in unique_strings {
-        println!("{}", string);
-    }
+    print_type_of(&x);
+    print_type_of(&y);
+    print_type_of(&z);
 }
