6d522f99415e16fc7c3fb0d7dd1692ba722c873d
Vera Kasianenko <112972833+CandyGoose@users.noreply.github.com>
2024-10-03 02:41:50 +0300

    l2.1

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,64 @@
-use std::collections::HashSet;
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, Read};
+use std::path::Path;
 
-fn all_unique_characters(input: &str) -> bool {
-    let mut seen = HashSet::new();
+fn main() -> io::Result<()> {
+    let args: Vec<String> = env::args().collect();
 
-    for ch in input.chars() {
-        let lower_ch = ch.to_lowercase().next().unwrap();
-        if seen.contains(&lower_ch) {
-            return false;
-        }
-        seen.insert(lower_ch);
+    if args.len() < 2 {
+        eprintln!("Usage: wc [option] <filename>");
+        return Ok(());
     }
 
-    true
+    let mut count_type = "words";
+    let mut file_name = "";
+
+    if args.len() == 2 {
+        file_name = &args[1];
+    } else if args.len() == 3 {
+        count_type = &args[1];
+        file_name = &args[2];
+    }
+
+    let path = Path::new(file_name);
+    let file = File::open(&path)?;
+
+    match count_type {
+        "-c" => {
+            let char_count = count_chars(file)?;
+            println!("{}", char_count);
+        }
+        "-l" => {
+            let line_count = count_lines(file)?;
+            println!("{}", line_count);
+        }
+        "-w" | _ => {
+            let word_count = count_words(file)?;
+            println!("{}", word_count);
+        }
+    }
+
+    Ok(())
 }
 
-fn main() {
-    let input1 = "abcd";
-    let input2 = "abCdefA";
-    let input3 = "aabcd";
+fn count_chars(mut file: File) -> io::Result<usize> {
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    Ok(contents.chars().count())
+}
 
-    println!("{} — {}", input1, all_unique_characters(input1));
-    println!("{} — {}", input2, all_unique_characters(input2));
-    println!("{} — {}", input3, all_unique_characters(input3));
+fn count_lines(file: File) -> io::Result<usize> {
+    let reader = io::BufReader::new(file);
+    Ok(reader.lines().count())
+}
+
+fn count_words(file: File) -> io::Result<usize> {
+    let reader = io::BufReader::new(file);
+    let mut word_count = 0;
+    for line in reader.lines() {
+        let line = line?;
+        word_count += line.split_whitespace().count();
+    }
+    Ok(word_count)
 }
